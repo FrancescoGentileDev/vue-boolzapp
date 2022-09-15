7fe68b2d411ccd4503ff8d59d46a9625b2277831
svalvastic <assogames2@gmail.com>
2022-09-15 18:50:21 +0200

    ✨ feat(script): send and reply

--- javascript/script.js
+++ javascript/script.js
@@ -1,10 +1,20 @@
 import contacts from './data.js';
 
+
+class Message {
+    constructor(message, sent) {
+        this.message = message;
+        this.sent = sent;
+        this.date = new Date()
+    }
+}
+
 const app = new Vue({
     el: "#app",
     data: {
         contacts,
-        activeContact: 0
+        activeContact: 0,
+        userInput: "",
     },
     methods: {
         getMessageHour: (message) => {
@@ -15,5 +25,20 @@ const app = new Vue({
            return (this.activeContact === index) ? "selected" : ""
         },
 
+        sendMessage() {
+            const message = new Message(this.userInput, true);
+            this.contacts[this.activeContact].messages.push(message)
+            this.userInput = ""
+            this.getReply(this.activeContact)
+        },
+        getReply(contact) {
+
+            const randReply = ["ok😅", "va bene😋", 'ottimo👍', "non vedo l'ora😍", 'certo! 🤗', 'grazie 1000! 😅']
+            const randomNumber = Math.floor(Math.random() * (randReply.length - 1))
+            setTimeout(() => {
+            const message = new Message(randReply[randomNumber], false);
+            this.contacts[contact].messages.push(message)
+            }, 2000);
+        },
     },
 })
